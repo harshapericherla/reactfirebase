b045f2c60ecbf110ff269e71c13b4ed557b3f825
harshapericherla <harsha.pericherla@gmail.com>
2019-07-18 14:21:11 -0400

    added Thunk for the async support

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import {connect} from 'react-redux';
+import { createProject } from '../store/action/ProjectActions';
 class CreateProject extends Component {
 
     state = {
@@ -15,7 +16,7 @@ class CreateProject extends Component {
 
    handleSubmit = (e) => {
        e.preventDefault();
-       console.log(this.state);
+       this.props.createProject(this.state);
    }
 
     render() {
@@ -40,4 +41,10 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject : (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(null,mapDispatchToProps)(CreateProject);
